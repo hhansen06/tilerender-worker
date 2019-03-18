1f97e6339daab3434f3859de55d9baa904992be8
hhansen06 <hhansen06@googlemail.com>
2019-03-18 12:57:40 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,24 +12,9 @@ if [ ! -f  $WORKOSM_DIR/shape.poly ]; then
         wget https://download.geofabrik.de/europe/germany/niedersachsen.poly -O $WORKOSM_DIR/shape.poly
 fi
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-
 osmosis --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change $WORKOSM_DIR/pipline.osc
-
 /usr/bin/python /home/renderer/src/regional/trim_osc.py -d gis -p "${WORKOSM_DIR}/shape.poly" $WORKOSM_DIR/pipline.osc $WORKOSM_DIR/pipline.nds.osc
-
 osm2pgsql --append -s -e15-20  -o $WORKOSM_DIR/expired_shapes  -C 300 -G --hstore --style /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -d gis $WORKOSM_DIR/pipline.nds.osc
-
-render_expired --min-zoom=10 --delete-from=10 --map=ajt < $WORKOSM_DIR/expired_shapes
-
-if [ ! -f /home/renderer/prerenderer_run/home/renderer/prerenderer_runningning ]; then
-touch /home/renderer/prerenderer_running
-
-render_list -a -z 1 -Z 7 -m ajt
-
-rm /home/renderer/prerenderer_running
-fi
 
 rm $WORKOSM_DIR/pipline.osc
 rm $WORKOSM_DIR/pipline.nds.osc
