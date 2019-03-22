886c744d1ccba35debde6e35a810551b795001af
hhansen06 <hhansen06@googlemail.com>
2019-03-22 12:51:08 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,19 +3,25 @@ echo "Updating OSM Database ..."
 cd ~/src/openstreetmap-carto
 WORKOSM_DIR=/home/renderer/osmosis_workdir
 
+if [[ ! -e $WORKOSM_DIR ]]; then
+    mkdir $WORKOSM_DIR
+fi
+
+
 if [ ! -f  $WORKOSM_DIR/state.txt ]; then
         wget "https://replicate-sequences.osm.mazdermind.de/?"`date -u +"%Y-%m-%d"`"T00:00:00Z" -O $WORKOSM_DIR/state.txt
         osmosis --read-replication-interval-init workingDirectory=$WORKOSM_DIR
+        sed -i 's!baseUrl=http://planet.openstreetmap.org/!baseUrl=https://planet.openstreetmap.org/!' $WORKOSM_DIR/configuration.txt
 fi
 
 if [ ! -f  $WORKOSM_DIR/shape.poly ]; then
         wget https://download.geofabrik.de/europe/germany/niedersachsen.poly -O $WORKOSM_DIR/shape.poly
 fi
-
+echo "Downloading changeset ..."
 osmosis --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change $WORKOSM_DIR/pipline.osc
-/usr/bin/python /home/renderer/src/regional/trim_osc.py -d gis -p "${WORKOSM_DIR}/shape.poly" $WORKOSM_DIR/pipline.osc $WORKOSM_DIR/pipline.nds.osc
-osm2pgsql --append -s -e15-20  -o $WORKOSM_DIR/expired_shapes  -C 300 -G --hstore --style /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -d gis $WORKOSM_DIR/pipline.nds.osc
-
+echo "Trimming to Niedersachsen ..."
+/usr/bin/python /home/renderer/src/regional/trim_osc.py -d gis -p "${WORKOSM_DIR}/shape.poly" $WORKOSM_DIR/pipline.osc $WORKOSM_DIR/pipline.osc
+echo "Importing changes to pgsql..."
+osm2pgsql --append -s -C 300 -G --hstore --style /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -d gis $WORKOSM_DIR/pipline.osc
+echo "Cleaing up ..."
 rm $WORKOSM_DIR/pipline.osc
-rm $WORKOSM_DIR/pipline.nds.osc
-rm $WORKOSM_DIR/expired_shapes
